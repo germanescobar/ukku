d94751fbf566c370162e9bd75242941de07b56e7
German Escobar <german.escobarc@gmail.com>
2015-01-25 02:36:55 -0500

    Implement vars:set command

---
+++ cli/lib/ukku/set_var_command.rb
@@ -0,0 +1,28 @@
+class SetVarCommand
+  def execute(args)
+    var_name = args['VAR_NAME']
+    var_value = args['VAR_VALUE']
+
+    data = YAML.load_file(UKKU_FILE)
+    name, server = data.first
+    if name.nil?
+      raise "No application configured. Run 'ukku configure <host>' first."
+    end
+
+    if data.length > 1
+      if args['--app'].empty?
+        raise "No app specified, use the --app NAME option"
+      else
+        name = args[--app]
+        sever = data[name]
+      end
+    end
+
+    host = server['host']
+    user = server['user']
+    identity_file = server['identity_file']
+
+    conn = Connection.new(host, user, identity_file)
+    conn.execute("echo '#{var_value}' > vars/#{var_name}")
+  end
+end
