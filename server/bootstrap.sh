33f6cf61bbafe83b5a39de115d307ce0fcc95330
German Escobar <german.escobarc@gmail.com>
2015-01-25 03:28:56 -0500

    Improve bootstrap.sh script

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -22,26 +22,68 @@ cd /usr/bin
 chmod 755 gitreceive
 gitreceive init
 
+# configure receiver script
 receiver_path="/home/git/receiver"
 cat > "$receiver_path" <<EOF
 #!/usr/bin/env bash
 
 cat | buildstep app
-docker kill app
-docker rm app
-docker run -d --name app -p 80:3000 -e DATABASE_URL="postgres://app:5tgbnhy6@postgres/app" -e PORT=3000 --link postgres:postgres app /bin/bash -c "/start web"
+launchapp
 EOF
 chmod +x "$receiver_path"
 chown git "$receiver_path"
 
+# configure launch script
+launch_path="/usr/bin/launchapp"
+cat > "$launch_path" <<EOF
+#!/usr/bin/env bash
+
+vars=""
+FILES=/etc/vars/*
+for f in $FILES
+do
+  vars="$vars -e ${f##*/}=$(<$f)"
+done
+
+docker kill app
+docker rm app
+docker run -d --name app -p 80:3000 $vars -e PORT=3000 --link postgres:postgres app /bin/bash -c "/start web"
+EOF
+chmod +x "$launch_path"
+chown git "$launch_path"
+
+# configure run command
+run_path="/usr/bin/runcommand"
+cat > "$run_path" <<EOF
+#!/usr/bin/env bash
+
+vars=""
+FILES=/etc/vars/*
+for f in $FILES
+do
+  vars="$vars -e ${f##*/}=$(<$f)"
+done
+
+command="/exec $@"
+docker run -t -i $vars --link postgres:postgres app /bin/bash -c "$command"
+EOF
+chmod +x "$run_path"
+
+# install authorized key
 cd
 key="$(cat .ssh/authorized_keys)"
 install_authorized_key "$key" "default"
+
+# install buildstep
 git clone https://github.com/progrium/buildstep.git
 cd buildstep
 make build
 cp buildstep /usr/bin/
 
+# give the group docker to the git user
 sudo usermod -a -G docker git
 
 docker run --name postgres -e POSTGRES_USER=app -e POSTGRES_PASSWORD=5tgbnhy6 -v /home/git/volumes/data:/var/lib/postgresql/data -d postgres
+
+mkdir -p /etc/vars
+echo postgres://app:5tgbnhy6@postgres/app > /etc/vars/DATABASE_URL
--- server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-command="/exec $@"
-docker run -t -i -e DATABASE_URL=postgres://app:5tgbnhy6@postgres/app --link postgres:postgres app /bin/bash -c "$command"
